cb62e640891d391d542b816501cce9f0bb7bee4a
Svetlana <gubinas83@gmail.com>
2020-10-13 17:55:57 +0300

    feat: implement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort(matrix) {
     if (arguments.length === 0) {
         return [];
@@ -9,5 +6,18 @@ module.exports = function towelSort(matrix) {
     if (Array.isArray(matrix) && matrix.length === 0) {
         return [];
     }
-    return [];
+
+    const even = (arr) => arr;
+    const odd = (arr) => arr.reverse();
+    let result = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i === 0 || i % 2 === 0) {
+            result.push(even(matrix[i]));
+        } else {
+            result.push(odd(matrix[i]))
+        }
+    }
+
+    return result.flat();
 }
